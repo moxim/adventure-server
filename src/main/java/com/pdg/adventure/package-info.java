c36a7220e2f99bc29cc24edd9c38e2095b9664ff
Michael Wiedmer <Michael.Wiedmer@finnova.com>
2022-10-28 17:06:32 +0200

    re-evaluate ideas

--- src/main/java/com/pdg/adventure/package-info.java
+++ src/main/java/com/pdg/adventure/package-info.java
@@ -4,17 +4,18 @@ package com.pdg.adventure;
  * TODO:
  *  - extend parser
  *    - add word type PREPOSTION
- *    - add second nound
+ *    - add second noun
  *    - add adverb
  *    - allow conjunctions through AND and THEN
  *    - allow speech (to interact with other characters, for instance SAY "Hello" )
  *    - allow all objects to be referenced by IT, thus, remember previous noun(s?)
  *    - add error type to applyCommand
- *  - add special locations
- *    - objects not available in any location
- *    - objects worn
- *  - add FollowupActions to PreConditions
+ *  - add special locations (workaround exist)
+ *    - objects not available in any location (no, use flags)
+ *    - objects worn (no, use objects)
+ *  - add FollowupActions to PreConditions (no, current design is sufficient)
+ *  - add serialization via https://github.com/nitrite/nitrite-java
  *
- *  For more, see: https://worldofspectrum.org//pub/sinclair/games-info/p/ProfessionalAdventureWriter_IntroductionManual.pdf
+ *  For more ideas, see: https://worldofspectrum.org//pub/sinclair/games-info/p/ProfessionalAdventureWriter_IntroductionManual.pdf
  *
  */
