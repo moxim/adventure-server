4803a47e7f4208f874e7d8d555e3364169d0f4b9
Michael Wiedmer <Michael.Wiedmer@pdg-software.com>
2026-05-29 08:01:39 +0200

    feat: add EqualsConditionMapper with tests

---
+++ src/main/java/com/pdg/adventure/server/mapper/condition/EqualsConditionMapper.java
@@ -0,0 +1,33 @@
+package com.pdg.adventure.server.mapper.condition;
+
+import org.springframework.stereotype.Service;
+
+import com.pdg.adventure.model.condition.EqualsConditionData;
+import com.pdg.adventure.server.annotation.AutoRegisterMapper;
+import com.pdg.adventure.server.condition.EqualsCondition;
+import com.pdg.adventure.server.support.MapperSupporter;
+
+@Service
+@AutoRegisterMapper(priority = 20, description = "EqualsCondition mapper")
+public class EqualsConditionMapper extends PreConditionMapper<EqualsConditionData, EqualsCondition> {
+
+    public EqualsConditionMapper(MapperSupporter aMapperSupporter) {
+        super(aMapperSupporter);
+        aMapperSupporter.registerMapper(EqualsConditionData.class, EqualsCondition.class, this);
+    }
+
+    @Override
+    public EqualsCondition mapToBO(EqualsConditionData data) {
+        EqualsCondition result = new EqualsCondition(data.getVariableName(), data.getValue(),
+                                                     getMapperSupporter().getVariableProvider());
+        result.setId(data.getId());
+        return result;
+    }
+
+    @Override
+    public EqualsConditionData mapToDO(EqualsCondition condition) {
+        EqualsConditionData result = new EqualsConditionData(condition.getVariableName(), condition.getValue());
+        result.setId(condition.getId());
+        return result;
+    }
+}
