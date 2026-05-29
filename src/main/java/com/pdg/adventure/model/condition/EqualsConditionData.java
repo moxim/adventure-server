c33cc43bec205431bfd45eaf813145126080710e
Michael Wiedmer <Michael.Wiedmer@pdg-software.com>
2026-05-29 15:50:57 +0200

    refactor: make EqualsConditionData fields mutable for editor support

--- src/main/java/com/pdg/adventure/model/condition/EqualsConditionData.java
+++ src/main/java/com/pdg/adventure/model/condition/EqualsConditionData.java
@@ -1,11 +1,13 @@
 package com.pdg.adventure.model.condition;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 public class EqualsConditionData extends PreConditionData {
-    private final String variableName;
-    private final String value;
+    private String variableName;
+    private String value;
 }
