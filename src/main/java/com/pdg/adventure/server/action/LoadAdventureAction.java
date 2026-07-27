04f3e908eaa8394ff1c04e1c770e4898257a3f54
Michael Wiedmer <Michael.Wiedmer@pdg-software.com>
2026-07-27 12:15:12 +0200

    add system messages

--- src/main/java/com/pdg/adventure/server/action/LoadAdventureAction.java
+++ src/main/java/com/pdg/adventure/server/action/LoadAdventureAction.java
@@ -20,6 +20,7 @@ import com.pdg.adventure.server.exception.ReloadAdventureException;
 import com.pdg.adventure.server.location.Location;
 import com.pdg.adventure.server.mapper.AdventureMapper;
 import com.pdg.adventure.server.parser.CommandExecutionResult;
+import com.pdg.adventure.server.storage.message.MessagesHolder;
 import com.pdg.adventure.server.storage.service.AdventureService;
 
 public class LoadAdventureAction extends AbstractAction {
@@ -71,6 +72,7 @@ public class LoadAdventureAction extends AbstractAction {
         }
 
         adventureConfig.allMessages().clear();
+        registerEngineMessages(adventureConfig.allMessages());
         for (MessageData messageData : adventureData.getMessages().values()) {
             adventureConfig.allMessages().addMessage(messageData.getMessageId(), messageData.getText());
         }
@@ -115,6 +117,22 @@ public class LoadAdventureAction extends AbstractAction {
         gameContext.setWorkflowData(adventureData.getWorkflowData());
 
         throw new ReloadAdventureException("Adventure reloaded, restarting game...");
+    }
+
+    // Generic, adventure-independent outcome messages that Action classes look up by fixed id
+    // (WearAction "-6", RemoveAction "-7", MoveItemAction "-8"/"-9", InventoryAction "-10",
+    // DestroyAction "-11", CreateAction "-12") - authors never define these themselves, so they
+    // must be re-seeded every time allMessages is cleared, regardless of which adventure is
+    // loaded. Mirrors the subset of MiniAdventureContent.setUpMessages() that isn't specific to
+    // the console's built-in demo adventure.
+    private void registerEngineMessages(MessagesHolder aMessagesHolder) {
+        aMessagesHolder.addMessage("-6", "You can't wear %s.");
+        aMessagesHolder.addMessage("-7", "You can't remove %s.");
+        aMessagesHolder.addMessage("-8", "The %s is full.");
+        aMessagesHolder.addMessage("-9", "You put %s into %s.");
+        aMessagesHolder.addMessage("-10", "You carry:");
+        aMessagesHolder.addMessage("-11", "The %s evaporates into thin air.");
+        aMessagesHolder.addMessage("-12", "A %s appears in the %s.");
     }
 
     private void listAdventures() {
