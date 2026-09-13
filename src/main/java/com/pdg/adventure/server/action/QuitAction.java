60c506c5f28b58a6c2d268b2e4ec030bb13d534b
Michael Wiedmer <Michael.Wiedmer@pdg-software.com>
2026-09-13 13:54:37 +0200

    remove empty c'tor

--- src/main/java/com/pdg/adventure/server/action/QuitAction.java
+++ src/main/java/com/pdg/adventure/server/action/QuitAction.java
@@ -7,8 +7,6 @@ import com.pdg.adventure.server.exception.QuitException;
 
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 public class QuitAction extends AbstractAction {
-    public QuitAction() {
-    }
 
     @Override
     public ExecutionResult execute() {
